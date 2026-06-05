cdb43bdbff0fecf52062eeb745c82e1268ac09db
Saif-09 <saifmd238@gmail.com>
2026-06-05 16:15:10 +0530

    Initial Commit

---
+++ insomniac/insomniacApp.swift
@@ -0,0 +1,17 @@
+//
+//  insomniacApp.swift
+//  insomniac
+//
+//  Created by Saif Siddiqui on 05/06/26.
+//
+
+import SwiftUI
+
+@main
+struct insomniacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
